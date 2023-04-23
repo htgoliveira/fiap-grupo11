22dc6ac3f2e795ff67b3bd7a9ef90e17188f475c
Humberto Oliveira <htgoliveira@outlook.com>
2023-04-23 20:45:07 -0300

    Teste Homol v3

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - v1"
+    return "Laboratório Pipeline DevOps - v1 - Homolog"
 
 
 if __name__ == '__main__':
